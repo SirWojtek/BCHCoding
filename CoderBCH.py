cd8d2eb7a478da789aadfcfbbb3e21855afd0760
Hubert <hubertwe@interia.pl>
2014-11-25 23:29:43 +0100

    Decoder added and example for 255 bit code

--- CoderBCH.py
+++ CoderBCH.py
@@ -1,44 +1,95 @@
 #!/usr/bin/python
 from Polynomial import *
 from copy import deepcopy
+import numpy
 
 class CoderBCH:
 
-	def __init__(self, generator, m):
-		self._generator = generator
-		self._m = m
-		self._n = 2 ** m - 1;
-		self._nk = self._generator.getPolyDegree()
-		self._k = self._n - self._nk
+    def __init__(self, generator, m, t):
+        self._generator = generator
+        self._m = m
+        self._n = 2 ** m - 1;
+        self._nk = self._generator.getPolyDegree()
+        self._k = self._n - self._nk
+        self._t = t
 
-	def encode(self, info):
-		if not self._isEncodePossible(info):
-			raise RuntimeError("Encoding imposible. Check length of message")
-		return self._encode(info)
+    def encode(self, info):
+        if not self._isEncodePossible(info):
+            raise RuntimeError("Encoding imposible. Check length of message")
+        return self._encode(info)
 
-	def _isEncodePossible(self, info):
-		return info.getPolyDegree() <= self._k
+    def decode(self, info):
+        msg = self._decode(info)
+        msg.divideByX(self._nk)
+        return msg
 
-	def _encode(self, info):
-		encMessage = deepcopy(info)
-		encMessage.multiplyByX(self._nk)
-		remainder = encMessage.divideRemainder(self._generator)
-		encMessage.add(remainder)
-		return encMessage
+    def _isEncodePossible(self, info):
+        return info.getPolyDegree() <= self._k
 
-	def __repr__(self):
-		return """Coder parameters:
+    def _encode(self, info):
+        encMessage = deepcopy(info)
+        encMessage.multiplyByX(self._nk)
+        remainder = encMessage.divideRemainder(self._generator)
+        encMessage.add(remainder)
+        return encMessage
+
+    def _decode(self, info):
+        decMessage = deepcopy(info)
+        for i in range(self._k):
+            syndrome = decMessage.divideRemainder(self._generator)
+            hammingWeight = self._getHammingWeight(syndrome)
+            if hammingWeight <= self._t:
+                print 'DECODE ITERATIONS: ' + str(i)
+                decMessage.add(syndrome)
+                decMessage.shiftLeft(i)
+                return decMessage
+            decMessage.shiftRight()
+        raise TypeError('Unable to correct errors for input message: ' + str(bin(info.getNumber())))
+
+
+    def _getHammingWeight(self, polynomial):
+        weight = 0
+        for bit in polynomial.getPoly():
+            if bit > 0:
+                weight += 1
+        return weight
+
+    def __repr__(self):
+        return """Coder parameters:
 generator:\t%s
 m:\t\t%d
 n:\t\t%d
 k:\t\t%d
 n - k:\t\t%d
-""" % (self._generator, self._m, self._n, self._k, self._nk)
+t:\t\t%d
+""" % (self._generator, self._m, self._n, self._k, self._nk, self._t)
+
+def addNoise(message, maxErrors):
+    poly = message.getPoly()
+    positions = numpy.random.randint(len(poly), size=maxErrors)
+    positions = list(set(positions))
+    for position in positions:
+        poly[position] = int(not(poly[position]))
+    return Polynomial(poly=poly)
+
 
 if __name__ == '__main__':
-	gen = Polynomial(0b10011)
-	info = Polynomial(0xA)
-	coder = CoderBCH(gen, 4)
-	print coder
-	msg = coder.encode(info)
-	print msg
+    t = 30
+    m = 8
+    gen = Polynomial(010754475055163544325315217357707003666111726455267613656702543301)
+    info = Polynomial(0b101011100110101110011010111001101011100110101110011010111001010)
+    coder = CoderBCH(gen, m, t)
+    print coder
+    print 'INFO: ' + str(info)
+    encodedMsg = coder.encode(info)
+    print 'ENCODED: ' + str(encodedMsg)
+    #Have some problem with generate proper noise. 
+    noisedMsg = addNoise(encodedMsg, 4)
+    #noisedMsg = encodedMsg
+    print 'NOISED: ' + str(noisedMsg)
+    decodedMsg = coder.decode(noisedMsg)
+    print 'DECODED: ' + str(decodedMsg)
+    if decodedMsg.getNumber() == info.getNumber():
+        print 'INFO and DECODED messages match!'
+    else:
+        print 'No match at all'
--- Polynomial.py
+++ Polynomial.py
@@ -1,75 +1,94 @@
 #!/usr/bin/python
 from numpy.polynomial import polynomial
+import numpy
 
 class Polynomial:
 
-	def __init__(self, number = 0):
-		self._poly = self._getPolynomial(number)
-		self._number = number
+    def __init__(self, number = 0, poly = None):
+        if poly is not None:
+            self._poly = poly
+            self._number = self._getNumberFromPoly(poly)
+        else:
+            self._poly = self._getPolynomial(number)
+            self._number = number
+            self._number = number
 
-	def getPoly(self):
-		return self._poly
+    def getPoly(self):
+        return list(self._poly)
 
-	def getNumber(self):
-		return self._number
+    def getNumber(self):
+        return self._number
 
-	def divideRemainder(self, binaryNumber):
-		resultPoly = polynomial.polydiv(self._poly, binaryNumber._poly)[1]
-		resultPoly = self._normalizePoly(resultPoly)
+    def divideRemainder(self, binaryNumber):
+        resultPoly = polynomial.polydiv(self._poly, binaryNumber._poly)[1]
+        resultPoly = self._normalizePoly(resultPoly, len(self._poly))
 
-		result = Polynomial()
-		result._number = self._getNumberFromPoly(resultPoly)
-		result._poly = resultPoly
+        result = Polynomial()
+        result._number = self._getNumberFromPoly(resultPoly)
+        result._poly = resultPoly
 
-		return result
+        return result
 
-	def getPolyDegree(self):
-		return len(self._poly) - 1
+    def getPolyDegree(self):
+        return len(self._poly) - 1
 
-	def multiplyByX(self, count):
-		self._poly = [0] * count + self._poly
-		self._number = self._getNumberFromPoly(self._poly)
+    def multiplyByX(self, count):
+        self._poly = [0] * count + self._poly
+        self._number = self._getNumberFromPoly(self._poly)
 
-	def add(self, binaryNumber):
-		result = polynomial.polyadd(
-			self._poly, binaryNumber._poly)
-		result = self._normalizePoly(result)
+    def divideByX(self, count):
+        self._poly = self._poly[count:]
+        self._number = self._getNumberFromPoly(self._poly)
 
-		self._poly = result
-		self._number = self._getNumberFromPoly(self._poly)
+    def add(self, binaryNumber):
+        result = polynomial.polyadd(
+            self._poly, binaryNumber._poly)
+        result = self._normalizePoly(result, len(self._poly))
+        self._poly = result
+        self._number = self._getNumberFromPoly(self._poly)
 
+    def shiftLeft(self, times=1):
+        self._poly = list(numpy.roll(self._poly, times))
+        self._number = self._getNumberFromPoly(self._poly)
 
-	def _getNumberFromPoly(self, poly):
-		number = 0
-		power = 0
-		for digit in reversed(poly):
-			number += 2 ** power * int(digit)
-			power += 1
-		return number
+    def shiftRight(self, times=1):
+        self._poly = list(numpy.roll(self._poly, -times))
+        self._number = self._getNumberFromPoly(self._poly)
 
-	def _normalizePoly(self, poly):
-		norm = [abs(int(i)) % 2 for i in poly]
-		return norm
+    def _getNumberFromPoly(self, poly):
+        number = 0
+        power = 0
+        for digit in poly:
+            number += 2 ** power * int(digit)
+            power += 1
+        return number
 
-	def _getPolynomial(self, number):
-		reversedDigit = []
-		for digit in reversed(self._translateToBinaryList(number)):
-			reversedDigit.append(digit)
-		return reversedDigit		
+    def _normalizePoly(self, poly, expectedSize):
+        norm = [abs(int(i)) % 2 for i in poly]
+        sizeDiff = expectedSize - len(norm)
+        if sizeDiff > 0:
+            norm += [0]*sizeDiff
+        return norm
 
-	def _translateToBinaryList(self, number):
-		return [int(d) for d in str(bin(number))[2:]]
+    def _getPolynomial(self, number):
+        reversedDigit = []
+        for digit in reversed(self._translateToBinaryList(number)):
+            reversedDigit.append(digit)
+        return reversedDigit        
 
-	def __repr__(self):
-		return self._poly.__repr__()
+    def _translateToBinaryList(self, number):
+        return [int(d) for d in str(bin(number))[2:]]
+
+    def __repr__(self):
+        return self._poly.__repr__()
 
 if __name__ == '__main__':
-	a = Polynomial(13)
-	b = Polynomial(14)
-	print a
-	print b
-	div = a.divideRemainder(b)
-	print div
+    a = Polynomial(13)
+    b = Polynomial(14)
+    print a
+    print b
+    div = a.divideRemainder(b)
+    print div
 
-	a.add(b)
-	print a
+    a.add(b)
+    print a
